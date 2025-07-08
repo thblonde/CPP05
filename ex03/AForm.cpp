a7c5940b37e6d72e9bd542ed5d9d7b2ef03482af
thblonde <blondet.thomas@protonmail.com>
2025-07-08 18:31:08 +0200

    fix exceptions

--- ex03/AForm.cpp
+++ ex03/AForm.cpp
@@ -85,3 +85,13 @@ void AForm::execute(Bureaucrat const & executor) const
         throw GradeTooLowException();
     }
 }
+
+const char* AForm::GradeTooHighException::what() const throw()
+{
+    return "Grade too high !";
+}
+
+const char* AForm::GradeTooLowException::what() const throw()
+{
+    return "Grade too low !";
+}
--- ex03/Bureaucrat.hpp
+++ ex03/Bureaucrat.hpp
@@ -27,19 +27,13 @@ class Bureaucrat
         class GradeTooHighException : public std::exception
         {
             public:
-                virtual const char* what() const throw()
-                {
-                    return "Grade too high !";
-                }
+                virtual const char* what() const throw();
         };
 
         class GradeTooLowException : public std::exception
         {
             public:
-                virtual const char* what() const throw()
-                {
-                    return "Grade too low !";
-                }
+                virtual const char* what() const throw();
         };
 
     private:
